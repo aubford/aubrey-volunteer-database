a737d29ee79774b133ecac513f819640d2aa3d6f
Ford <aford@celerity.com>
2019-08-07 16:47:09 -0600

    remove TrustProxies

--- app/Http/Middleware/TrustProxies.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Middleware;
-use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
-class TrustProxies extends Middleware
-{
-    protected $proxies = '*';
-    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
-}
